d3c250eddbbee2dfe44c60a856f9f8db022df8cd
Drew Davis <drewdavis@drews-mbp.myfiosgateway.com>
2021-06-26 12:10:14 -0400

    added dorpdown to html

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -19,7 +19,25 @@ tableData.forEach(function(ufoSighting) {
       cell.text(value);
     });
   });
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 // select the button
 var button = d3.select("#filter-btn");
