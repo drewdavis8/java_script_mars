7d889093764cc9643239a4019c5d1af184a69c5d
Drew Davis <drewdavis@drews-mbp.myfiosgateway.com>
2021-06-26 10:03:11 -0400

    updated code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,56 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// from data.js
+var tableData = data;
+console.log(tableData);
+
+// get a reference to the table body
+var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+
+    // use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// DAS button!
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // select the input date get the raw HTML nodes
+    var inputElement = d3.select("#datetime");
+    // get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // console.log input value
+    console.log(inputValue);
+    // filter Data with datetime equal to input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log filter values
+    console.log(filteredData);
+
+
+    filteredData.forEach(function(selections) {
+
+    console.log(selections);
+    // append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+    // use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // append cells to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,59 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// refer to the table body
+var tbody = d3.select("tbody");
+
+// UFO sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// select the button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // select the input date, state, shape and get the raw HTML nodes
+    var inputElement = d3.select("#input");
+    // get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // console.log input value
+    // console.log(inputValue);
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
+                                                    sighting.city === inputValue ||
+                                                    sighting.state === inputValue ||
+                                                    sighting.country === inputValue ||
+                                                    sighting.shape === inputValue);
+    // console.log filter values
+    console.log(filteredData);
+
+
+    filteredData.forEach(function(selections) {
+
+    console.log(selections);
+    // append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+    // use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
